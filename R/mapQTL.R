3442ef53a938caba82092676fe93c8f5d1e2f935
Selcan Aydin <selcant@gmail.com>
2024-10-09 09:07:21 -0400

    updating calc_kinship cores to 1, we don't need many cores to run this step. it is quick anyways.

--- R/mapQTL.R
+++ R/mapQTL.R
@@ -65,13 +65,13 @@ mapQTL <- function(outdir, peaks_out, map_out, genoprobs, samp_meta, expr_mats, 
   }
 
   ## Modify Probs and Determine Kinship
-  if( is.null(total_cores)) total_cores <- get_cores()
+  #if( is.null(total_cores)) total_cores <- get_cores()
   qtlprobs <- list()
   kinship_loco <- list()
   for (tissue in names(probs_list)) {
     message(tissue)
     qtlprobs[[tissue]] <- probs_3d_to_qtl2(probs_list[[tissue]])
-    kinship_loco[[tissue]] <- qtl2::calc_kinship(qtlprobs[[tissue]], "loco", cores = total_cores)
+    kinship_loco[[tissue]] <- qtl2::calc_kinship(qtlprobs[[tissue]], "loco", cores = 4)
   }
 
   ## Create maps
