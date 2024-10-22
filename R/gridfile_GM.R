858510268f639e50fda9c6d92f75e3b063f26d67
Hannah Dewey <deweyh@sumner027.sumner2.jax.org>
2024-10-22 16:26:55 -0400

    Add GigaMUGA gridfile

---
+++ R/gridfile_GM.R
@@ -0,0 +1,18 @@
+#' Grid Map
+#'
+#' GigaMUGA grid map for mm39 genome assembly.
+#'
+#' @docType data
+#' @name gridfile_GM
+#' @format A 143259 x 4 data frame with marker names included as row names:
+#' \describe{
+#'   \item{chr}{Chromosome name}
+#'   \item{pos}{Marker position in basepairs}
+#'   \item{cM}{Marker position in centiMorgan}
+#'   \item{bp}{Marker position in basepairs}
+#' }
+#'
+#' @source https://raw.githubusercontent.com/kbroman/MUGAarrays/main/UWisc/gm_uwisc_v4.csv.
+#' @usage data(gridfile_GM)
+#'
+NULL
