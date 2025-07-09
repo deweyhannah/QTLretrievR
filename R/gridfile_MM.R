71d9f08afbbf02645343597f2ba098bd3e9d09de
Hannah Dewey <deweyh@sumner086.sumner2.jax.org>
2025-07-09 10:00:31 -0400

    Add miniMUGA

--- R/gridfile_MM.R
+++ R/gridfile_MM.R
@@ -4,14 +4,14 @@
 #'
 #' @docType data
 #' @name gridfile_MM
-#' @format A 11125 x 3 data frame with marker names included as row names:
+#' @format A 77808 x 3 data frame with marker names included as row names:
 #' \describe{
 #'   \item{chr}{Chromosome name}
 #'   \item{pos}{Marker position in basepairs}
 #'   \item{cM}{Marker position in centiMorgan}
 #' }
 #'
-#' @source https://raw.githubusercontent.com/kbroman/MUGAarrays/refs/heads/main/UWisc/mini_uwisc_v5.csv.
+#' @source https://raw.githubusercontent.com/kbroman/MUGAarrays/refs/heads/main/UWisc/mm_uwisc_v4.csv.
 #' @usage data(gridfile_MM)
 #'
 NULL
---
+++ R/gridfile_mini.R
@@ -0,0 +1,17 @@
+#' Grid Map
+#'
+#' miniMUGA genetic and physical map for mm39 genome assembly.
+#'
+#' @docType data
+#' @name gridfile_mini
+#' @format A 11125 x 3 data frame with marker names included as row names:
+#' \describe{
+#'   \item{chr}{Chromosome name}
+#'   \item{pos}{Marker position in basepairs}
+#'   \item{cM}{Marker position in centiMorgan}
+#' }
+#'
+#' @source https://raw.githubusercontent.com/kbroman/MUGAarrays/refs/heads/main/UWisc/mini_uwisc_v5.csv.
+#' @usage data(gridfile_mini)
+#'
+NULL
